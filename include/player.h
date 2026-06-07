08196d8d43dcccf699a72716f9428e827420c76d
ang3ltorres <ang3lstorres@gmail.com>
2026-06-07 11:53:57 -0600

    drill

--- include/player.h
+++ include/player.h
@@ -5,9 +5,13 @@
 typedef struct Player
 {
   Rectangle rect;
+  int dir;
   float velX;
   float velY;
   int coyoteCounter;
+  bool drill;
+  bool drillUsed;
+  int  drillDir;
 } Player;
 
 extern Player player;
--- src/player.c
+++ src/player.c
@@ -18,14 +18,37 @@ void playerInit()
   player = (Player)
   {
     .rect          = {32.0f, 32.0f, 6.0f, 15.0f},
+    .dir           = 1,
     .velX          = 0.0f,
     .velY          = 0.0f,
-    .coyoteCounter = 0
+    .coyoteCounter = 0,
+    .drill         = false,
+    .drillUsed     = false,
+    .drillDir      = 0
   };
 }
 
 void playerUpdate()
 {
+  //////////////
+  // controls //
+  //////////////
+  bool button_left  = IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A);
+  bool button_right = IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D);
+  bool button_up    = IsKeyDown(KEY_UP)   || IsKeyDown(KEY_W);
+  bool button_down  = IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S);
+
+  bool button_left_pressed  = IsKeyPressed(KEY_LEFT)  || IsKeyPressed(KEY_A);
+  bool button_right_pressed = IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D);
+  bool button_up_pressed    = IsKeyPressed(KEY_UP)    || IsKeyPressed(KEY_W);
+  bool button_down_pressed  = IsKeyPressed(KEY_DOWN)  || IsKeyPressed(KEY_S);
+
+  bool button_a = IsKeyDown(KEY_LEFT_SHIFT);
+  bool button_b = IsKeyDown(KEY_SPACE);
+
+  bool button_a_pressed = IsKeyPressed(KEY_LEFT_SHIFT);
+  bool button_b_pressed = IsKeyPressed(KEY_SPACE);
+
   /////////////
   // gravity //
   /////////////
@@ -57,20 +80,23 @@ void playerUpdate()
     player.coyoteCounter--;
 
   // Jump with coyote time
-  if (IsKeyPressed(KEY_SPACE) && player.coyoteCounter > 0)
+  if (button_b_pressed && player.coyoteCounter > 0)
   {
     player.velY = -JUMP_SPEED;
     player.coyoteCounter = 0;
   }
 
-  //////////////
-  // controls //
-  //////////////
+  /////////////////////////
+  // horizontal movement //
+  /////////////////////////
   int moveDir = 0;
-  if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
-    moveDir -= 1;
-  if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
-    moveDir += 1;
+
+  // Disable movement if drill
+  if (button_left)  { moveDir -= 1; player.dir = -1; }
+  if (button_right) { moveDir += 1; player.dir =  1; }
+
+  if (!player.velY && button_a)
+    moveDir = 0.0f;
 
   // If not moving, apply friction
   if (moveDir != 0)
@@ -94,8 +120,22 @@ void playerUpdate()
       player.rect.x = (float)((int)((player.rect.x + player.rect.width) / TILE_SIZE) * TILE_SIZE) - player.rect.width;
     else
       player.rect.x = (float)((int)(player.rect.x / TILE_SIZE) * TILE_SIZE + TILE_SIZE);
+
     player.velX = 0.0f;
   }
+
+  ///////////
+  // drill //
+  ///////////
+  player.drill    = button_a;
+  player.drillDir = player.dir;
+
+  if (player.velY)
+  {
+    if (button_up)   player.drillDir = -2;
+    if (button_down) player.drillDir =  2;
+  }
+
 }
 
 void playerDraw()
@@ -110,4 +150,17 @@ void playerDraw()
   );
   
   DrawRectangleRec(player.rect, MAGENTA);
+
+  // Draw drill
+  if (player.drill)
+  {
+    if      (player.drillDir ==  1)
+      DrawRectangle(player.rect.x + 6.5, player.rect.y + 6.5, 5, 4, BLUE);
+    else if (player.drillDir == -1)
+      DrawRectangle(player.rect.x - 4.5, player.rect.y + 6.5, 5, 4, BLUE);
+    else if (player.drillDir == -2)
+      DrawRectangle(player.rect.x + 1.5, player.rect.y - 4.5, 4, 5, BLUE);
+    else if (player.drillDir == 2)
+      DrawRectangle(player.rect.x + 1.5, player.rect.y + 15.5, 4, 5, BLUE);
+  }
 }
